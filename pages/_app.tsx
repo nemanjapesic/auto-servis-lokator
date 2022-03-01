ad09429f9d84bc184cce009f13bc5d491fb3b9c4
Nemanja Pesic <nemanja992@gmail.com>
2022-03-01 14:30:21 +0100

    add toast limit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import Head from 'next/head';
-import { Toaster } from 'react-hot-toast';
+import { useEffect } from 'react';
+import toast, { Toaster, useToasterStore } from 'react-hot-toast';
 import Layout from '../components/Layout';
 import { AuthProvider } from '../context/AuthContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
+  const { toasts } = useToasterStore();
+
+  const TOAST_LIMIT = 1;
+
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible) // Only consider visible toasts
+      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
+      .forEach((t) => toast.remove(t.id)); // Dismiss
+  }, [toasts]);
+
   return (
     <AuthProvider>
       <Toaster />
